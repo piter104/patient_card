e6ee6a06e925388fbe410fe09223215e455d693e
piter104 <piotr.derenowski@gmail.com>
2021-05-23 22:27:52 +0200

    Init Project

---
+++ src/main/java/com/joder/iwm/karta_pacjenta/KartaPacjentaApplication.java
@@ -0,0 +1,13 @@
+package com.joder.iwm.karta_pacjenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartaPacjentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartaPacjentaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joder/iwm/karta_pacjenta/KartaPacjentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joder.iwm.karta_pacjenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartaPacjentaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
